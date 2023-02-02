7bdf601910461b075c360f915837b8eeb839a14d
sagarsc07 <chiplunkarsagar18@gmail.com>
2023-02-02 20:36:34 +0530

    javascript

--- index.js
+++ index.js
@@ -1,38 +1,73 @@
-const secondsDiv = document.getElementById('seconds');
-const minutesDiv = document.getElementById('minutes');
-const hoursDiv = document.getElementById('hours');
-const ampmDiv = document.getElementById('ampm');
+//Sithika Buddy
 
-let seconds = new Date().getSeconds();
-let hours = new Date().getHours();
-let minutes = new Date().getMinutes();
+// const secondsDiv = document.getElementById('seconds');
+// const minutesDiv = document.getElementById('minutes');
+// const hoursDiv = document.getElementById('hours');
+// const ampmDiv = document.getElementById('ampm');
 
-setInterval(() => {
-  seconds++;
-  if (seconds > 60) {
-    seconds = 0;
-    minutes++;
-  }
-  if (minutes > 60) {
-    minutes = 0;
-    hours++;
-  }
-  if (minutes.toString().length < 2) {
-    minutes = '0' + minutes.toString();
-  }
-  if (seconds.toString().length < 2) {
-    seconds = '0' + seconds.toString();
-  }
-  if (hours.toString().length < 2) {
-    hours = '0' + hours.toString();
-  }
-  hoursDiv.innerText = hours;
-  minutesDiv.innerText = minutes;
-  secondsDiv.innerText = seconds;
-}, 1000);
+// let seconds = new Date().getSeconds();
+// let hours = new Date().getHours();
+// let minutes = new Date().getMinutes();
 
-if (hours > 12) {
-  ampmDiv.innerText = 'PM';
-} else {
-  ampmDiv.innerText = 'AM';
+// setInterval(() => {
+//   seconds++;
+//   if (seconds > 60) {
+//     seconds = 0;
+//     minutes++;
+//   }
+//   if (minutes > 60) {
+//     minutes = 0;
+//     hours++;
+//   }
+//   if (minutes.toString().length < 2) {
+//     minutes = '0' + minutes.toString();
+//   }
+//   if (seconds.toString().length < 2) {
+//     seconds = '0' + seconds.toString();
+//   }
+//   if (hours.toString().length < 2) {
+//     hours = '0' + hours.toString();
+//   }
+//   hoursDiv.innerText = hours;
+//   minutesDiv.innerText = minutes;
+//   secondsDiv.innerText = seconds;
+// }, 1000);
+
+// if (hours > 12) {
+//   ampmDiv.innerText = 'PM';
+// } else {
+//   ampmDiv.innerText = 'AM';
+// }
+
+// Javascript King YT
+
+const hourEl = document.getElementById('hours');
+const minuteEl = document.getElementById('minutes');
+const secondEl = document.getElementById('seconds');
+const ampmEl = document.getElementById('ampm');
+
+function updateClock() {
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let ampm = 'AM';
+
+  if (h > 12) {
+    h = h - 12;
+    ampm = 'PM';
+  }
+
+  h = h < 10 ? '0' + h : h;
+  m = m < 10 ? '0' + m : m;
+  s = s < 10 ? '0' + s : s;
+
+  hourEl.innerText = h;
+  minuteEl.innerText = m;
+  secondEl.innerText = s;
+  ampmEl.innerText = ampm;
+
+  setTimeout(() => {
+    updateClock();
+  }, 1000);
 }
+updateClock();
