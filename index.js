3660c07ce9a38adec02b9b0869d8f8ed8e10f9cf
Sagar S Chiplunkar <106554264+Sagarsc07@users.noreply.github.com>
2023-02-02 20:38:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
+// Javascript King YT
+
+const hourEl = document.getElementById('hours');
+const minuteEl = document.getElementById('minutes');
+const secondEl = document.getElementById('seconds');
+const ampmEl = document.getElementById('ampm');
+
+function updateClock() {
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let ampm = 'AM';
+
+  if (h > 12) {
+    h = h - 12;
+    ampm = 'PM';
+  }
+
+  h = h < 10 ? '0' + h : h;
+  m = m < 10 ? '0' + m : m;
+  s = s < 10 ? '0' + s : s;
+
+  hourEl.innerText = h;
+  minuteEl.innerText = m;
+  secondEl.innerText = s;
+  ampmEl.innerText = ampm;
+
+  setTimeout(() => {
+    updateClock();
+  }, 1000);
+}
+updateClock();
+
+
 //Sithika Buddy
 
 // const secondsDiv = document.getElementById('seconds');
@@ -38,36 +72,3 @@
 // } else {
 //   ampmDiv.innerText = 'AM';
 // }
-
-// Javascript King YT
-
-const hourEl = document.getElementById('hours');
-const minuteEl = document.getElementById('minutes');
-const secondEl = document.getElementById('seconds');
-const ampmEl = document.getElementById('ampm');
-
-function updateClock() {
-  let h = new Date().getHours();
-  let m = new Date().getMinutes();
-  let s = new Date().getSeconds();
-  let ampm = 'AM';
-
-  if (h > 12) {
-    h = h - 12;
-    ampm = 'PM';
-  }
-
-  h = h < 10 ? '0' + h : h;
-  m = m < 10 ? '0' + m : m;
-  s = s < 10 ? '0' + s : s;
-
-  hourEl.innerText = h;
-  minuteEl.innerText = m;
-  secondEl.innerText = s;
-  ampmEl.innerText = ampm;
-
-  setTimeout(() => {
-    updateClock();
-  }, 1000);
-}
-updateClock();
